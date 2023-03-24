c68f0ad55dbeae02583b818ed6467fd829afa660
zs12341 <rewin6455@gmail.com>
2023-03-24 11:00:16 +0900

    initial commit

---
+++ src/main/java/com/example/cloneburgerking/CloneburgerkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloneburgerking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneburgerkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneburgerkingApplication.class, args);
+    }
+
+}
