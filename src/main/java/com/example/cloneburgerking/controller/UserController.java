3d80db0ab30eb813cd89e442026118b41674d243
sanggyu <limsanggyu@kakao.com>
2023-03-24 14:42:34 +0900

    #1 회원가입, Exception

---
+++ src/main/java/com/example/cloneburgerking/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.example.cloneburgerking.controller;
+
+import com.example.cloneburgerking.dto.LoginRequestDto;
+import com.example.cloneburgerking.dto.SignupRequestDto;
+import com.example.cloneburgerking.service.UserService;
+import com.example.cloneburgerking.status.CustomException;
+import com.example.cloneburgerking.status.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+@RestController
+@Validated
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+
+//    @ApiOperation(value = "회원가입 테스트", notes = "회원가입 테스트")
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@Validated @RequestBody SignupRequestDto signupRequestDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomException(ErrorCode.NOT_FOUND_SIGNUP_USER);
+        }
+        return userService.signup(signupRequestDto);
+    }
+
+//    @ApiOperation(value = "로그인 테스트", notes = "로그인 테스트")
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Validated @RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        return userService.login(loginRequestDto, response);
+    }
+}
---
+++ src/main/java/com/example/cloneburgerking/status/ErrorCode.java
@@ -0,0 +1,40 @@
+package com.example.cloneburgerking.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    NOT_TOKEN(HttpStatus.BAD_REQUEST, "로그인이 필요합니다."),
+
+    /* 400 BAD_REQUEST : 잘못된 요청 */
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "토큰이 유효하지 않습니다"),
+
+
+    /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
+    INVALID_AUTH_TOKEN(HttpStatus.UNAUTHORIZED, "권한 정보가 없는 토큰입니다"),
+    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "현재 내 계정 정보가 존재하지 않습니다"),
+
+    /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저 정보를 찾을 수 없습니다"),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "로그아웃 된 사용자입니다"),
+
+    /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
+    NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다."),
+
+    NOT_FOUND_USER_ID(HttpStatus.BAD_REQUEST, "이미 가입된 아이디가 존재합니다."),
+
+    NOT_FOUND_BOARD_ADMIN(HttpStatus.BAD_REQUEST, "게시물을 찾을 수 없습니다.(관리자계정)"),
+
+    NOT_FOUND_SIGNUP_USER(HttpStatus.BAD_REQUEST, "회원가입 형식이 맞지 않습니다."),
+
+    HEART_Not_found_Exception(HttpStatus.NOT_FOUND, "좋아요 기록을 찾을 수 없습니다.커밋용 !!!!");
+
+
+
+    private final HttpStatus httpStatus;
+    private final String detail;
+
+}
