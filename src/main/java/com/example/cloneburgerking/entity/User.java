7c448589a9ab37a0c19ab4f479b769082f6f3fab
sanggyu <limsanggyu@kakao.com>
2023-03-26 21:25:54 +0900

    nginX 1

--- src/main/java/com/example/cloneburgerking/entity/User.java
+++ src/main/java/com/example/cloneburgerking/entity/User.java
@@ -26,9 +26,27 @@ public class User {
     @Enumerated(value = EnumType.STRING)
     private UserEnum role;
 
+//    private Long kakaoId;
+
+//    @Column(nullable = false, unique = true)
+//    private String email;
+
     public User(String username, String password, UserEnum role) {
         this.username = username;
         this.password = password;
         this.role = role;
     }
+
+    public User(String nickname, Long kakaoId, String encodedPassword, String email, UserEnum user) {
+        this.username = username;
+//        this.kakaoId = kakaoId;
+        this.password = password;
+//        this.email = email;
+        this.role = role;
+    }
+
+//    public User kakaoIdUpdate(Long kakaoId) {
+//        this.kakaoId = kakaoId;
+//        return this;
+//    }
 }
