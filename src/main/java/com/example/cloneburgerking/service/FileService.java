e5eed23ea284c47444d57ce203dce6c937612ec0
zs12341 <rewin6455@gmail.com>
2023-03-27 17:47:14 +0900

    fix : s3_image_bucket / nginx test 25

--- src/main/java/com/example/cloneburgerking/service/FileService.java
+++ src/main/java/com/example/cloneburgerking/service/FileService.java
@@ -7,6 +7,7 @@ import com.example.cloneburgerking.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.List;
 @Service
 @RequiredArgsConstructor
