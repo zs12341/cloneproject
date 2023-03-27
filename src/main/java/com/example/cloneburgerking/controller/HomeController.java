5c27c695a15e42d2f25c7aa52529f5770fd83763
zs12341 <rewin6455@gmail.com>
2023-03-27 11:46:19 +0900

    fix : html *4 / nginx test 17

--- src/main/java/com/example/cloneburgerking/controller/HomeController.java
+++ src/main/java/com/example/cloneburgerking/controller/HomeController.java
@@ -1,12 +1,11 @@
 package com.example.cloneburgerking.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class HealthCheckController {
+public class HomeController {
 
     @GetMapping("/health")
     public ModelAndView home() {
