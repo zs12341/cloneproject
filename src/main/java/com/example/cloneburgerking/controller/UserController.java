32de0c0d6e3bdd8856c363a23964904d085d522c
DESKTOP-19A4V8P\82105 <salsb119@gmail.com>
2023-03-28 22:15:27 +0900

    feat : Kakao 로그인

--- src/main/java/com/example/cloneburgerking/controller/UserController.java
+++ src/main/java/com/example/cloneburgerking/controller/UserController.java
@@ -2,15 +2,20 @@ package com.example.cloneburgerking.controller;
 
 import com.example.cloneburgerking.dto.LoginRequestDto;
 import com.example.cloneburgerking.dto.SignupRequestDto;
+import com.example.cloneburgerking.jwt.JwtUtil;
+import com.example.cloneburgerking.kakao.KakaoService;
 import com.example.cloneburgerking.service.UserService;
 import com.example.cloneburgerking.status.CustomException;
 import com.example.cloneburgerking.status.ErrorCode;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 
@@ -22,20 +27,6 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/signup")
-    public ModelAndView signupPage() {
-        return new ModelAndView("signup");
-    }
-
-    @GetMapping("/index")
-    public ModelAndView home() {
-    return new ModelAndView("index");
-}
-
-    @GetMapping("/login-page")
-    public ModelAndView loginPage() {
-    return new ModelAndView("login");
-}
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@Validated @RequestBody SignupRequestDto signupRequestDto, BindingResult bindingResult) {
@@ -51,17 +42,5 @@ public class UserController {
         return userService.login(loginRequestDto, response);
     }
 
-    @GetMapping("/callback")
-    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
-        // code: 카카오 서버로부터 받은 인가 코드
-        String createToken = kakaoService.kakaoLogin(code, response);
-
-        // Cookie 생성 및 직접 브라우저에 Set
-        Cookie cookie = new Cookie(jwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
-        cookie.setPath("/");
-        response.addCookie(cookie);
-
-        return "redirect:/index";
-    }
 
 }
