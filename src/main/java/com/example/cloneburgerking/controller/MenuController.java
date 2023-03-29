b5c23f21632f31c9c1dc56d944db12479d023ea3
sanggyu <limsanggyu@kakao.com>
2023-03-29 11:11:27 +0900

    feat: 폼데이터 log추가

--- src/main/java/com/example/cloneburgerking/controller/MenuController.java
+++ src/main/java/com/example/cloneburgerking/controller/MenuController.java
@@ -7,9 +7,11 @@ import com.example.cloneburgerking.dto.SecurityExceptionDto;
 import com.example.cloneburgerking.security.UserDetailsImpl;
 import com.example.cloneburgerking.service.MenuService;
 import com.example.cloneburgerking.service.S3Service;
-import com.example.cloneburgerking.status.ErrorCode;
 import groovyjarjarantlr4.v4.runtime.misc.Nullable;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +22,13 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequiredArgsConstructor
 public class MenuController {
     private final MenuService menuService;
     private final S3Service s3Service;
+    private static final Logger logger = LoggerFactory.getLogger(MenuController.class);
 
 
     //S3 업로드
@@ -61,7 +65,7 @@ public class MenuController {
                                         @Nullable @RequestPart("requestDto")  MenuRequestDto requestDto,
                                         @Nullable @RequestPart("file")  MultipartFile file,
                                         @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
-
+        try {
         if (file.isEmpty()) {
             menuService.textUpdate(id, requestDto, userDetails.getUser());
             SecurityExceptionDto securityExceptionDto = new SecurityExceptionDto("텍스트 수정 성공!", HttpStatus.OK.value());
@@ -71,7 +75,12 @@ public class MenuController {
 
         SecurityExceptionDto securityExceptionDto = new SecurityExceptionDto("수정 성공!", HttpStatus.OK.value());
         return ResponseEntity.status(HttpStatus.OK).body(securityExceptionDto);
-    }
+        } catch (Exception e) {
+            // 예외 발생 시 로그 출력
+            logger.error("Error occurred while updating menu: {}", e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error occurred while updating menu");
+        }
+        }
 
 
 }
